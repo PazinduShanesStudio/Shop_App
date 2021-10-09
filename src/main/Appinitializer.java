e4c2e169c2ed472ad83663cc8866a48d46985563
pazindushane <shanepazindu7399@gmail.com>
2021-10-09 10:34:30 +0530

    first commmit added

---
+++ src/main/Appinitializer.java
@@ -0,0 +1,11 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Appinitializer extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
